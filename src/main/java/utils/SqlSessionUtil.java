d2bf8f81dc5c121805ac66af6438c014474decf5
TanEn <TanEn@qq.com>
2022-09-09 00:57:17 +0800

    FIRST COMMIT BY TANEN

---
+++ src/main/java/utils/SqlSessionUtil.java
@@ -0,0 +1,36 @@
+package utils;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+
+/**
+ * @author:谭恩翔
+ * @create: 2022-08-02 17:54
+ * @Description:
+ */
+public class SqlSessionUtil {
+    public static SqlSession getSqlSession() {
+        SqlSession sqlSession = null;
+        try {
+            //读取mybatis的核心配置文件
+            InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
+            //创建SqlSessionFactoryBuilder对象
+            SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
+            //通过核心配置文件对应的字节输入流创建工厂类SqlSessionFactory,生产sqlsession对象
+            SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBuilder.build(is);
+            //true设置为默认自动提交。不待true是默认手动提交
+            sqlSession = sqlSessionFactory.openSession(true);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return sqlSession;
+    }
+}
